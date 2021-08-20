da0b9ce40ce8f51eebbd3bf1707b6c6cf10dade6
Emre Keskin <emrekeskin60@gmail.com>
2021-08-21 01:45:45 +0300

    First Simple Calculator
    First Simple Calculator

--- FirstSimpleCalculator/ViewController.swift
+++ FirstSimpleCalculator/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFirst: UITextField!
+    @IBOutlet weak var txtSecond: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var lblRed: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func btnSum(_ sender: Any) {
+        
+        if let firstNumb = Double(txtFirst.text!) {
+            if let secondNumb = Double(txtSecond.text!) {
+                let result = firstNumb + secondNumb
+                lblResult.text = String(result)
+            } else {
+                lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+            }
+        } else {
+            lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+        }
+    }
+    
+    @IBAction func btnMinus(_ sender: Any) {
+        if let firstNumb = Double(txtFirst.text!) {
+            if let secondNumb = Double(txtSecond.text!) {
+                let result = firstNumb - secondNumb
+                lblResult.text = String(result)
+            } else {
+                lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+            }
+        } else {
+            lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+        }
+    }
+    
+    @IBAction func btnMultiply(_ sender: Any) {
+        if let firstNumb = Double(txtFirst.text!) {
+            if let secondNumb = Double(txtSecond.text!) {
+                let result = firstNumb * secondNumb
+                lblResult.text = String(result)
+            } else {
+                lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+            }
+        } else {
+            lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+        }
+    }
+    
+    @IBAction func btnDivide(_ sender: Any) {
+        if let firstNumb = Double(txtFirst.text!) {
+            if let secondNumb = Double(txtSecond.text!) {
+                let result = firstNumb / secondNumb
+                lblResult.text = String(result)
+            } else {
+                lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+            }
+        } else {
+            lblRed.text = "Lütfen Sayı Girişi Yapınız!"
+        }
+    }
 }
 
