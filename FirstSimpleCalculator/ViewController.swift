d0998392603848b2142778f133281e64de6dcd07
Emre Keskin <emrekeskin60@gmail.com>
2021-08-21 00:54:20 +0300

    Initial Commit

---
+++ FirstSimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstSimpleCalculator
+//
+//  Created by Emre Keskin on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
